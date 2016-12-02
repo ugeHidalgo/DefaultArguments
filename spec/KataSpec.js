9f793e12cb43520c27c81fc3195f385814db35ee
ugeHidalgo <ugeHidalgo@hotmail.com>
2016-12-02 22:17:09 +0100

    Use of currying to default function

--- spec/KataSpec.js
+++ spec/KataSpec.js
@@ -12,4 +12,33 @@ describe('Kata', function() {
     it('add function should works', function() {
       expect(add(2,3)).toEqual(5);
     });
+
+    it('defaultArguments should not default arguments when 0 arguments', function() {
+      var newAddFunction = defaultArguments(add);      
+      expect(newAddFunction(3,7)).toEqual(10);
+    });
+
+    it('defaultArguments should default argument b', function() {
+      var newAddFunction = defaultArguments(add, {b:13});      
+      expect(newAddFunction(3)).toEqual(16);
+    });
+
+    it('defaultArguments should default two arguments', function() {
+      var newAddFunction = defaultArguments(add,{a:10, b:20});      
+      expect(newAddFunction()).toEqual(30);
+    });
+
+    it('defaultArguments should pass kata examples 1', function() {
+      var newAddFunction = defaultArguments(add,{b:9});      
+      expect(newAddFunction(10)).toEqual(19);
+      expect(newAddFunction(10,5)).toEqual(15);
+    });
+
+    it('defaultArguments should pass kata examples 2', function() {
+      var newAddFunction = defaultArguments(add,{b:9});      
+      expect(newAddFunction(10)).toEqual(19);
+      expect(newAddFunction(10,5)).toEqual(15);
+    });
+
+
 });
--- src/Kata.js
+++ src/Kata.js
@@ -1,3 +1,17 @@
-function defaultArguments(func, params) {
-  // TODO: Program me
+function defaultArguments(func) {
+  var args = [];
+  
+  if (arguments.length > 1) {
+    if (arguments[1].hasOwnProperty('a')) {
+        args.push (arguments[1].a);
+    }  
+    if (arguments[1].hasOwnProperty('b')) {
+        args.push (arguments[1].b);
+    }
+  }   
+
+  return function() {
+      var funcArgs = args.concat(Array.prototype.slice.call(arguments,0));
+      return func.apply(this,funcArgs);
+  }
 }
